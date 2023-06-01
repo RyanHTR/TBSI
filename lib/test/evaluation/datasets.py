dfbf35c0dd0119ccad4782fc366d9fe5e950053f
Zizheng Xun <lakerslife@sina.com>
2023-06-01 09:43:53 +0800

    ✨ feat:  Added support for the rgbt210 dataset.

--- lib/test/evaluation/datasets.py
+++ lib/test/evaluation/datasets.py
@@ -27,6 +27,7 @@ dataset_dict = dict(
                                        kwargs=dict()),
     lasher_test=DatasetInfo(module=pt % "lasher", class_name="LasHeRDataset", kwargs=dict(split='testingset')),
     rgbt234 = DatasetInfo(module=pt % "rgbt234", class_name="RGBT234Dataset", kwargs=dict(split='testingset')),
+    rgbt210 = DatasetInfo(module=pt % "rgbt210", class_name="RGBT210Dataset", kwargs=dict(split='testingset')),
 )
 
 
